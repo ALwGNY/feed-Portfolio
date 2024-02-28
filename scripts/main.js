ea32b1937aba1efa24b3ea36923090c6f7ec922f
ALwGNY <128140409+ALwGNY@users.noreply.github.com>
2024-02-28 12:25:04 +0100

    supr submodule

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const categories = document.querySelectorAll(".home-main__category");
+
+  categories.forEach(function (category) {
+    category.addEventListener("click", function () {
+      const categoryId = category.id;
+
+      const allCategoryContents = document.querySelectorAll(
+        ".home-main__card-feed"
+      );
+      allCategoryContents.forEach(function (content) {
+        content.classList.remove("active");
+      });
+
+      const selectedCategoryContent = document.getElementById(
+        `${categoryId}-content`
+      );
+      selectedCategoryContent.classList.add("active");
+
+      categories.forEach(function (cat) {
+        cat.classList.remove("active");
+      });
+      category.classList.add("active");
+    });
+  });
+
+  const defaultCategory = document.getElementById("feed");
+  defaultCategory.click();
+});
