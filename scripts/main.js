052342951475a7628bf5dec2df930d60b77bf210
ALwGNY <128140409+ALwGNY@users.noreply.github.com>
2024-03-15 15:25:24 +0100

    section expertise

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const categoryId = category.id;
 
       const allCategoryContents = document.querySelectorAll(
-        ".home-main__card-feed"
+        ".home-main_section"
       );
       allCategoryContents.forEach(function (content) {
         content.classList.remove("active");
@@ -24,6 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  const defaultCategory = document.getElementById("feed");
+  const defaultCategory = document.getElementById("expetises");
   defaultCategory.click();
 });
